18d10bda4f3b01d0f6ff6abe0fbf7c15c17d427e
yashtotla <yashtotla98@gmail.com>
2024-08-23 12:00:42 +0530

    :hammer: add shadcn collapsible component

--- @/components/extension.tsx
+++ @/components/extension.tsx
@@ -1,7 +1,13 @@
+import { Collapsible } from "./ui/collapsible";
+
 export default function Extension() {
   return (
-    <main>
-      <h1>Reimagine YouTube</h1>
+    <main className={`antialiased w-full mb-3 z-10`}>
+      <div className="w-full">
+        <Collapsible className="space-y-3">
+          <h1>Reimagine YouTube</h1>
+        </Collapsible>
+      </div>
     </main>
   )
 }
